e4bdef931b8d863b70d729a7ae2dd17dbdc09997
Ming Shen <saintshen@gmail.com>
2017-04-24 23:50:50 -0400

    add identity

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace web.Controllers
+namespace webapp.Controllers
 {
     public class HomeController : Controller
     {
